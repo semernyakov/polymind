66ff8c3324a202b5078810f54b882448f9b75f9b
Иван Семерняков <beatuminflow@gmail.com>
2026-04-14 08:59:39 +0300

    chore: remove unused commented exports and format code
    - Remove commented exports from src/types/index.ts
    - Remove blank lines before eslint-disable comments in GroqChatView.tsx

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,11 +3,4 @@ export * from './api';
 export * from './plugin';
 export * from './types';
 export * from './settings';
-// export * from './logger';
 export * from './views';
-// export * from './chat';
-
-// export interface AuthState {
-//   isAuthenticated: boolean;
-//   error?: string;
-// }
